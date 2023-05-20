16c64104ad2c34609ecdfca4baee5140dda4181d
devpedroshd <pedroshd7@gmail.com>
2023-05-19 23:19:48 -0300

    iniciado o front e back end das paginas de dados dos pacientes

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.messages import constants
+from .models import Pacientes
 
 
 @login_required(login_url='/auth/logar')
 def pacientes(request):
     if request.method == "GET":
-        return render(request, "pacientes.html")
+        pacientes = Pacientes.objects.filter(nutri = request.user)
+        return render(request, 'pacientes.html', {"pacientes": pacientes})
     elif request.method == "POST":
         nome = request.POST.get('nome')
         sexo = request.POST.get('sexo')
@@ -14,5 +18,50 @@ def pacientes(request):
         email = request.POST.get('email')
         telefone = request.POST.get('telefone')
 
+    if (len(nome.strip()) == 0) or (len(sexo.strip()) == 0) or (len(idade.strip()) == 0) or  (len(email.strip()) == 0) or  (len(telefone.strip()) == 0):
+        messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+        return redirect('/pacientes/')
     
-        return HttpResponse(f"{nome}, {sexo}, {idade}, {telefone}")
+    if not idade.isnumeric():
+        messages.add_message(request, constants.ERROR, 'Digite uma idade válida')
+        return redirect('/pacientes/')
+    
+    pacientes = Pacientes.objects.filter(email=email)
+
+    if pacientes.exists():
+        messages.add_message(request, constants.ERROR, 'Ja existe um paciente com esse e-mail')
+        return redirect('/pacientes/')
+    
+
+
+    try:
+        p1 = Pacientes(
+            nome = nome,
+            sexo = sexo,
+            idade = idade,
+            email = email,
+            telefone = telefone,
+            nutri = request.user
+        )
+
+        p1.save()
+        messages.add_message(request, constants.SUCCESS, 'Ja existe um paciente com esse e-mail')
+        return redirect('/pacientes/')
+    
+    except Exception as e:
+            return HttpResponse(f"Ocorreu um erro no sistema: {str(e)}")
+    
+
+
+@login_required(login_url = '/auth/logar/')
+def dados_paciente_listar(request):
+     if request.method == "GET":
+        pacientes = Pacientes.objects.filter(nutri = request.user)
+        return render(request, 'dados_paciente_listar.html', {'pacientes': pacientes}) 
+     
+        
+     
+
+@login_required(login_url='auth/logar/')
+def dados_paciente(request, id):
+    return HttpResponse(id)
