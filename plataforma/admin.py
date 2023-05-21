81fb405371b172f0986edff47e1e0330f74e2b73
devpedroshd <pedroshd7@gmail.com>
2023-05-20 21:53:11 -0300

    Feito a pagina com os dados de peso,altura e etc com grafico demonstrativo

--- plataforma/admin.py
+++ plataforma/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Pacientes
+from .models import Pacientes, DdosPaciente
 
 admin.site.register(Pacientes)
+admin.site.register(DdosPaciente)
--- plataforma/views.py
+++ plataforma/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.messages import constants
-from .models import Pacientes
+from .models import Pacientes, DdosPaciente
+from django.contrib.auth.models import User
+from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
 
 
 @login_required(login_url='/auth/logar')
 def pacientes(request):
     if request.method == "GET":
-        pacientes = Pacientes.objects.filter(nutri = request.user)
+        pacientes = Pacientes.objects.filter(nutri=request.user)
         return render(request, 'pacientes.html', {"pacientes": pacientes})
     elif request.method == "POST":
         nome = request.POST.get('nome')
@@ -18,19 +21,17 @@ def pacientes(request):
         email = request.POST.get('email')
         telefone = request.POST.get('telefone')
 
-    if (len(nome.strip()) == 0) or (len(sexo.strip()) == 0) or (len(idade.strip()) == 0) or  (len(email.strip()) == 0) or  (len(telefone.strip()) == 0):
-        messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
-        return redirect('/pacientes/')
-    
-    if not idade.isnumeric():
-        messages.add_message(request, constants.ERROR, 'Digite uma idade válida')
-        return redirect('/pacientes/')
-    
-    pacientes = Pacientes.objects.filter(email=email)
+        if (len(nome.strip()) == 0) or (len(sexo.strip()) == 0) or (len(idade.strip()) == 0) or (len(email.strip()) == 0) or (len(telefone.strip()) == 0):
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            return redirect('/pacientes/')
 
-    if pacientes.exists():
-        messages.add_message(request, constants.ERROR, 'Ja existe um paciente com esse e-mail')
-        return redirect('/pacientes/')
+        if not idade.isnumeric():
+            messages.add_message(request, constants.ERROR, 'Digite uma idade válida')
+            return redirect('/pacientes/')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Já existe um usuário com esse e-mail')
+            return redirect('/cadastro/')
     
 
 
@@ -45,7 +46,7 @@ def pacientes(request):
         )
 
         p1.save()
-        messages.add_message(request, constants.SUCCESS, 'Ja existe um paciente com esse e-mail')
+        messages.add_message(request, constants.SUCCESS, 'Usuario Cadastrado com sucesso')
         return redirect('/pacientes/')
     
     except Exception as e:
@@ -64,4 +65,57 @@ def dados_paciente_listar(request):
 
 @login_required(login_url='auth/logar/')
 def dados_paciente(request, id):
-    return HttpResponse(id)
+    paciente = get_object_or_404(Pacientes, id=id)
+    if not paciente.nutri == request.user:
+        messages.add_message(request, constants.ERROR, 'Esse paciente não é seu')
+        return redirect('/dados_paciente/')
+        
+    if request.method == "GET":
+        dados_paciente = DdosPaciente.objects.filter(paciente = paciente)
+        return render(request, 'dados_paciente.html', {'paciente': paciente, 'dados_paciente': dados_paciente})
+    elif request.method == "POST":
+        peso = request.POST.get('peso')
+        altura = request.POST.get('altura')
+        gordura = request.POST.get('gordura')
+        musculo = request.POST.get('musculo')
+        hdl = request.POST.get('hdl')
+        ldl = request.POST.get('ldl')
+        colesterol_total = request.POST.get('ctotal')
+        triglicerídios = request.POST.get('triglicerídios')
+
+    
+    if peso.strip() == '' or altura.strip() == '' or gordura.strip() == '' or musculo.strip() == '' or hdl.strip() == '' or ldl.strip() == '' or colesterol_total.strip() == '' or triglicerídios.strip() == '':
+        messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+        return redirect('/dados_paciente/')
+
+    paciente = DdosPaciente(paciente = paciente,
+                                data = datetime.now(),
+                                peso = peso,
+                                altura = altura,
+                                percentual_gordura = gordura,
+                                percentual_musculo = musculo,
+                                colesterol_hdl = hdl,
+                                colesterol_ldl = ldl,
+                                colesterol_total = colesterol_total,
+                                trigliceridios=triglicerídios)
+    paciente.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Dados cadastrados com sucesso')
+    
+
+    return render(request, 'dados_paciente.html', {'paciente': paciente})
+
+
+@login_required(login_url='/auth/logar/')
+@csrf_exempt
+def grafico_peso(request, id):
+    paciente = Pacientes.objects.get(id=id)
+    dados = DdosPaciente.objects.filter(paciente = paciente).order_by("data")
+
+    pesos = [dado.peso for dado in dados]
+    labels = list(range(len(pesos)))
+    data = {'peso': pesos,
+            'labels': labels}
+    return JsonResponse(data)
+
+        
